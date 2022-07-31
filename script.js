c17c0789c4d2500182a85f38b1ec45c3fd42efb9
Adriana Darris <adrianadarris@Adrianas-Air.lan>
2022-07-31 17:11:27 -0500

    corrected error

--- script.js
+++ script.js
@@ -7,18 +7,17 @@ window.addEventListener("load", function(){
     formSubmission(document);
 
 
-   let listedPlanets;
+   let listedPlanet;
    
    let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
+       listedPlanet = result;
    }).then(function () {
-       console.log(listedPlanets);
 
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-       listedPlanets = listedPlanets[pickPlanet()];
-       addDestinationInfo();
+       listedPlanet = listedPlanet[pickPlanet()];
+       console.log(listedPlanet.image);
+       addDestinationInfo(document, listedPlanet.name, listedPlanet.diameter, listedPlanet.star, listedPlanet.distance, listedPlanet.moons, listedPlanet.image)
     })
   
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -5,21 +5,18 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
    // Here is the HTML formatting for our mission target div.
    document = window.document;
    let mission = document.getElementById("missionTarget");
-   let json = myFetch();
-   let i = 3;
         mission.innerHTML = `
-                <h2>Mission Destination</h2>
-                <ol>
-                    <li>Name: ${json.name}</li>
-                    <li>Diameter: ${json.diameter}</li>
-                    <li>Star: ${json.star}</li>
-                    <li>Distance from Earth: ${json.distance}</li>
-                    <li>Number of Moons: ${json.moons}</li>
-                </ol>
-                <img src="${json.image}">
-    `;
-    
-};
+        <h2>Mission Destination</h2>
+        <ol>
+            <li>Name: ${name}</li>
+            <li>Diameter: ${diameter}</li>
+            <li>Star: ${star}</li>
+            <li>Distance from Earth: ${distance}</li>
+            <li>Number of Moons: ${moons}</li>
+        </ol>
+        <img src="${imageUrl}">
+        `;
+}
 
 
 function validateInput(testInput) {
